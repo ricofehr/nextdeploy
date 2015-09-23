de02e1c8e787bd9f346f9395917abb0fcdcd3a75
ricofehr <ricofehr@nextdeploy.io>
2015-09-23 17:27:13 +0200

    fix some issue with rvm install

--- vagrant/modules/pm/manifests/ror.pp
+++ vagrant/modules/pm/manifests/ror.pp
@@ -8,6 +8,11 @@
 # Eric Fehr <eric.fehr@publicis-modem.fr>
 #
 class pm::ror {
+  class{ 'rvm::rvmrc':
+    max_time_flag => 180,
+    before  => Class['rvm'],
+  }
+
   class { 'rvm': }
   $gems = hiera('rvm::gem', [])
   create_resources('rvm_gem', $gems)
