629e7bc807a9afb5c3fc292c74497e066fb964d6
erifeh <Eric.FEHR@publicis-modem.fr>
2015-10-11 14:14:18 +0200

    add some gitlab and mysql options

--- vagrant/modules/pm/manifests/gitlab7.pp
+++ vagrant/modules/pm/manifests/gitlab7.pp
@@ -10,6 +10,19 @@
 class pm::gitlab7 {
   $server_name = hiera('global::gitlabns', 'gitlab.local')
 
+  # prepare folders for git-data and backups
+  file { '/home/git-data':
+    ensure => directory,
+  } ->
+  exec { 'gitdatachown':
+    command => '/bin/chown git: /home/git-data',
+  } ->
+  file { '/home/gitlab-backups':
+    ensure => directory,
+  } ->
+  exec { 'gitbackupschown':
+    command => '/bin/chown git: /home/gitlab-backups',
+  } ->
   class { 'gitlab': }
   ->
   exec { 'gitlab_reconfigure_fixweirdbug':
@@ -17,7 +30,8 @@ class pm::gitlab7 {
     timeout     => 1800,
     logoutput   => true,
     tries       => 5,
-    require => Exec['gitlab_reconfigure']
+    require => Exec['gitlab_reconfigure'],
+    unless => 'test -f /home/modem/.gitlabconfig',
   } ->
   file_line { 'gitlab_servername':
     path => '/var/opt/gitlab/nginx/conf/gitlab-http.conf',
@@ -26,6 +40,11 @@ class pm::gitlab7 {
   } ->
   exec { 'restart_nginx':
     command => '/opt/gitlab/embedded/sbin/nginx -c /var/opt/gitlab/nginx/conf/nginx.conf -p /var/opt/gitlab/nginx/ -s reload',
-    user => 'root'
+    user => 'root',
+    unless => 'test -f /home/modem/.gitlabconfig',
+  } ->
+  exec { 'touch_gitlabconfig':
+    command => '/bin/touch /home/modem/.gitlabconfig',
+    user => 'modem'
   }
 }
