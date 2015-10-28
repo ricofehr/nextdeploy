f865278734bdc56d74d270fc658841c3037f1539
erifeh <Eric.FEHR@publicis-modem.fr>
2015-10-28 19:44:06 +0100

    Fix issue #5: wrong group displaying

--- ror/app/serializers/group_serializer.rb
+++ ror/app/serializers/group_serializer.rb
@@ -11,10 +11,10 @@ class GroupSerializer < ActiveModel::Serializer
   def users
     if current_user.admin?
       users_a = object.users.select { |u| u.id != current_user.id }
-      users_a.unshift(current_user)
+      users_a.unshift(current_user) if object.id == current_user.group.id
     elsif current_user.lead?
       users_a = object.users.select { |u| u.id != current_user.id && u.projects.any? { |project| project.users.include?(current_user) } }
-      users_a.unshift(current_user)
+      users_a.unshift(current_user) if object.id == current_user.group.id
     else
       [] << current_user
     end
