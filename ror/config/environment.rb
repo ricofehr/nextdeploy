cedc2e32c1d7002cb5732fe2f4976c8772ab03ac
ricofehr <ricofehr@nextdeploy.io>
2015-10-28 21:40:09 +0100

    Fix issue #4, set encoding to UTF8

--- ror/config/environment.rb
+++ ror/config/environment.rb
@@ -1,3 +1,7 @@
+# Default ruby encoding
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
