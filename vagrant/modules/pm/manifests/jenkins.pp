45a55d426a08eacec7f4b59d70a3a8e8c695819c
ricofehr <ricofehr@nextdeploy.io>
2015-11-16 17:54:12 +0100

    fix w3af and jenkins install

--- vagrant/modules/pm/manifests/jenkins.pp
+++ vagrant/modules/pm/manifests/jenkins.pp
@@ -9,9 +9,21 @@
 #
 class pm::jenkins {
 
+  file { '/etc/apt/sources.list.d/jenkins.list':
+    ensure => file,
+    content => 'deb http://pkg.jenkins-ci.org/debian binary/'
+  } ->
+
+  exec { 'aptupdatejenkins':
+    command => '/usr/bin/apt-get update',
+    timeout => 1800,
+    creates => '/usr/share/jenkins/jenkins.war'
+  } ->
+
   # jenkins service
   package { 'jenkins':
     ensure => present,
+    install_options => [ '-y', '--force-yes' ]
   } ->
 
   service { 'jenkins':
