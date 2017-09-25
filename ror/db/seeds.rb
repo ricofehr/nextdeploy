01ad43e74adb7990b645804c11e08e4aea2d0ff5
ricofehr <ricofehr@nextdeploy.io>
2017-09-25 17:31:33 +0200

    Add wordpress 4.8.2 cms

--- ror/db/seeds.rb
+++ ror/db/seeds.rb
@@ -116,7 +116,7 @@ framework_drupal8 =
 
 framework_wordpress =
   Framework.create!(
-    name: 'Wordpress-4.8.1',
+    name: 'Wordpress-4.8.2',
     publicfolder: '',
     rewrites: "RewriteEngine On\\nRewriteRule ^/?$ /index.php [L]\\n" +
               "RewriteCond %%{literal('%')}{REQUEST_URI} !=/server-status\\n" +
